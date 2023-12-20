7b34eb1951c3bc3047b0581beffb5c382f647332
yzays8 <yzays8@gmail.com>
2023-12-21 03:16:51 +0900

    Add setup files

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+readonly VENV_DIR='.venv'
+
+if [ ! -d "$VENV_DIR" ]; then
+    bash setup.sh
+fi
+
+source "$VENV_DIR/bin/activate"
+
+cd src
+if [[ -z "$@" ]]; then
+    python3 main.py
+else
+    python3 main.py "$@"
+fi
+deactivate
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eu
+
+readonly VENV_DIR='.venv'
+
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+fi
+
+source "$VENV_DIR/bin/activate"
+pip3 install -r requirements.txt
+deactivate
