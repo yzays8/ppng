cf213d778750f19cf6ec24f55e692f2212f472eb
yzays8 <yzays8@gmail.com>
2023-12-22 22:16:12 +0900

    Rename assert functions

--- tests/test_crc32.py
+++ tests/test_crc32.py
@@ -8,7 +8,7 @@ ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(ROOT_PATH)
 from src.pngd.crc32 import calculate_crc32
 
-def assert_crc32(data: bytes) -> None:
+def assert_equal_crc32(data: bytes) -> None:
     expected = zlib.crc32(data)
     actual = calculate_crc32(data)
     if expected != actual:
@@ -16,14 +16,14 @@ def assert_crc32(data: bytes) -> None:
 
 def test_crc32():
     try:
-        assert_crc32('IEND'.encode('utf-8'))
-        assert_crc32(b'')
-        assert_crc32(b'123456789')
-        assert_crc32(b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f')
-        assert_crc32('こんにちは世界'.encode('utf-8'))
-        assert_crc32(b'a' * 10**6)
+        assert_equal_crc32('IEND'.encode('utf-8'))
+        assert_equal_crc32(b'')
+        assert_equal_crc32(b'123456789')
+        assert_equal_crc32(b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f')
+        assert_equal_crc32('こんにちは世界'.encode('utf-8'))
+        assert_equal_crc32(b'a' * 10**6)
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=100))
-        assert_crc32(random_string.encode('utf-8'))
+        assert_equal_crc32(random_string.encode('utf-8'))
     except AssertionError as e:
         print(f'Failed: {e}')
         return
--- tests/test_decode.py
+++ tests/test_decode.py
@@ -9,19 +9,19 @@ from src.pngd.decoder import decode_png, is_logging
 
 TEST_DIR = os.path.join(ROOT_PATH, 'tests/image/mandrill')
 
-def assert_type(expected: np.ndarray, actual: np.ndarray) -> None:
+def assert_equal_type(expected: np.ndarray, actual: np.ndarray) -> None:
     if expected.dtype != actual.dtype:
         raise AssertionError(f'Type (expected: {expected.dtype}, actual: {actual.dtype})')
 
-def assert_shape(expected: np.ndarray, actual: np.ndarray) -> None:
+def assert_equal_shape(expected: np.ndarray, actual: np.ndarray) -> None:
     if expected.shape != actual.shape:
         raise AssertionError(f'Shape (expected: {expected.shape}, actual: {actual.shape})')
 
-def assert_data(expected: np.ndarray, actual: np.ndarray) -> None:
+def assert_equal_data(expected: np.ndarray, actual: np.ndarray) -> None:
     if not np.array_equal(expected, actual):
         raise AssertionError(f'Data')
 
-def assert_image(path: str) -> None:
+def assert_equal_image(path: str) -> None:
     expected = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     try:
         with open(path, 'rb') as f:
@@ -37,9 +37,9 @@ def assert_image(path: str) -> None:
             else:
                 # if dec_data is mono-color and shape is just (height, width)
                 actual = dec_data
-            assert_type(expected, actual)
-            assert_shape(expected, actual)
-            assert_data(expected, actual)
+            assert_equal_type(expected, actual)
+            assert_equal_shape(expected, actual)
+            assert_equal_data(expected, actual)
     except FileNotFoundError:
         raise FileNotFoundError(f'File not found: {os.path.abspath(path)}')
     except AssertionError as ae:
@@ -53,14 +53,14 @@ def test_decode():
         # assert_image(TEST_DIR + '/type0-1bit.png')
         # assert_image(TEST_DIR + '/type0-2bit.png')
         # assert_image(TEST_DIR + '/type0-4bit.png')
-        assert_image(TEST_DIR + '/type0-8bit.png')
-        assert_image(TEST_DIR + '/type0-16bit.png')
-        assert_image(TEST_DIR + '/type2-8bit.png')
-        assert_image(TEST_DIR + '/type2-16bit.png')
-        assert_image(TEST_DIR + '/type4-8bit.png')
-        assert_image(TEST_DIR + '/type4-16bit.png')
-        assert_image(TEST_DIR + '/type6-8bit.png')
-        assert_image(TEST_DIR + '/type6-16bit.png')
+        assert_equal_image(TEST_DIR + '/type0-8bit.png')
+        assert_equal_image(TEST_DIR + '/type0-16bit.png')
+        assert_equal_image(TEST_DIR + '/type2-8bit.png')
+        assert_equal_image(TEST_DIR + '/type2-16bit.png')
+        assert_equal_image(TEST_DIR + '/type4-8bit.png')
+        assert_equal_image(TEST_DIR + '/type4-16bit.png')
+        assert_equal_image(TEST_DIR + '/type6-8bit.png')
+        assert_equal_image(TEST_DIR + '/type6-16bit.png')
     except AssertionError as ae:
         print(f'Failed: {ae}')
     except Exception as e:
