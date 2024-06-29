4e34375fdd9b467532d0bc44b1b1ba54daa43cfb
yzays8 <yzays8@gmail.com>
2024-06-30 05:47:34 +0900

    Refactor main.py

--- src/main.py
+++ src/main.py
@@ -1,32 +1,44 @@
-import os
 import time
+from pathlib import Path
 
 import typer
+from rich.console import Console
+from rich.traceback import install
 
 import ppng
 
+console = Console()
+install(show_locals=True)
 
-def main(
-    file: str = typer.Argument(..., help="PNG file to decode"),
-    is_logging: bool = typer.Option(False, "-l", "--logging", help="Enable logging"),
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    file: Path = typer.Argument(
+        ...,
+        help="PNG file to decode",
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+    ),
+    logging: bool = typer.Option(False, "-l", "--logging", help="Enable logging"),
 ) -> None:
     try:
         with open(file, "rb") as f:
-            start = time.time()
-            image = ppng.Decoder(is_logging=is_logging).decode_png(f)
-            if is_logging:
-                print(f"Decoding time: {time.time() - start:.3f} sec")
-            ppng.show_image(image, os.path.basename(file))
-        return
-    except FileNotFoundError:
-        print(f"File not found: {os.path.abspath(file)}")
-    except IsADirectoryError:
-        print(f"Is a directory: {os.path.abspath(file)}")
-    except PermissionError:
-        print(f"Permission denied: {os.path.abspath(file)}")
+            st = time.perf_counter()
+            image = ppng.Decoder(is_logging=logging).decode_png(f)
+
+        if logging:
+            dt = time.perf_counter() - st
+            console.print(f"Decoding time: {dt:.3f} sec")
+
+        ppng.show_image(image, file.name)
     except Exception as e:
-        print(e)
+        console.print(f"[bold red]Unexpected error:[/] {e}")
+        raise typer.Exit(1)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    typer.run(run)
