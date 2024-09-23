89a261a43c831af94633e32f5d5bc2ebe88674fe
yzays8 <yzays8@gmail.com>
2024-09-23 19:26:50 +0900

    Make fields private in BitStream

--- src/ppng/utils/bitstream.py
+++ src/ppng/utils/bitstream.py
@@ -4,20 +4,20 @@ import sys
 
 class BitStream:
     def __init__(self, byte_stream: bytes) -> None:
-        self.stream = io.BytesIO(byte_stream)
-        self.byte = 0
-        self.bit_offset = 0
+        self._stream = io.BytesIO(byte_stream)
+        self._byte = 0
+        self._bit_offset = 0
 
     # Returns 1 bit from the data sliced byte by byte from the stream.
     # The bit is read from the LSB to the MSB of the byte.
     def read_bit(self) -> int:
-        if self.bit_offset == 0:
-            self.byte = self.stream.read(1)[0]
-        bit = self.byte & 0b1
-        self.byte >>= 1
-        self.bit_offset += 1
-        if self.bit_offset == 8:
-            self.bit_offset = 0
+        if self._bit_offset == 0:
+            self._byte = self._stream.read(1)[0]
+        bit = self._byte & 0b1
+        self._byte >>= 1
+        self._bit_offset += 1
+        if self._bit_offset == 8:
+            self._bit_offset = 0
         return bit
 
     # e.g. 0b01001011 -> 0b11010010 (reverse=True)
@@ -33,12 +33,12 @@ class BitStream:
 
     # If the position of the bit is not a multiple of 8, ignore the remaining bits and read the next byte.
     def read_byte(self, reverse: bool = True) -> int:
-        if self.bit_offset != 0:
-            self.bit_offset = 0
-        self.byte = self.stream.read(1)[0]
+        if self._bit_offset != 0:
+            self._bit_offset = 0
+        self._byte = self._stream.read(1)[0]
         if reverse == False:
-            return self.byte
-        return int("{:08b}".format(self.byte)[::-1], 2)
+            return self._byte
+        return int("{:08b}".format(self._byte)[::-1], 2)
 
     def read_bytes(self, length: int, reverse: bool = True, endian: str = "big") -> int:
         ret = 0
